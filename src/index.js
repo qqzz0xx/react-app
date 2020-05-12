bc05294ea7b1964cfb722a40a1bf9a2d9a43e7a8
sqz <632574418@qq.com>
2020-05-12 18:37:15 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import './bootstrap-theme-slate.css';
 
 ReactDOM.render(
   <React.StrictMode>
